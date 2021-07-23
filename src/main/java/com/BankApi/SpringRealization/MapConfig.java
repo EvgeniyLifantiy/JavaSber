061aca9873bdeba1e718e2bf493bc488c5f11046
Евгений <lifantii@inbox.ru>
2021-07-23 12:18:52 +0300

    Spring hash

--- src/main/java/com/BankApi/SpringRealization/MapConfig.java
+++ src/main/java/com/BankApi/SpringRealization/MapConfig.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapConfig {
+    //Default fictive realization
     Map<Class,Class>getMapOfImpl(){
         return new HashMap<Class,Class>(Map.of(BillDao.class, BillDaoImpl.class));
     }
